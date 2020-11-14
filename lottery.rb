ad6951248c04c9b3b920bfff36df621cda600ebe
Lucas Ethan <asmrit1010@gmail.com>
2020-11-14 08:01:05 +0100

    Made so it generates winning numbers and puts them in a winning numbers array

--- lottery.rb
+++ lottery.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 numbers = []
+winning_num = []
 6.times do |i|
   print "[#{i + 1}] Enter a number between 1 and 20: "
   numbers << gets.to_i
@@ -8,5 +9,5 @@ end
 
 puts "The winning numbers are:"
 6.times do |i|
-  puts "[#{i + 1}] #{rand(1..20)}"
+  puts "[#{i + 1}] #{winning_num << rand(1..20)}"
 end
